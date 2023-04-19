e4587cdb380f9d3ffe904fe0a454262d3699685c
htorricotecsup <53871912+htorrico@users.noreply.github.com>
2023-04-19 10:57:21 -0500

    Add project files.

---
+++ Demo052023A/Demo052023A/EntryCode.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo052023A
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryCode : ContentPage
+    {
+        public EntryCode()
+        {
+            InitializeComponent();
+
+            StackLayout stack=new StackLayout();
+            Entry entry=new Entry();
+            entry.Placeholder = "Entry por código";
+            stack.Children.Add(entry);
+
+            for (int i = 0; i < 5; i++)
+            {
+
+                Button button = new Button();
+                button.Text = "Demo";
+                stack.Children.Add(button);
+            }
+            
+            
+            this.Content = stack;
+
+
+
+
+        }
+    }
+}
---
+++ Demo052023A/Demo052023A/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Demo052023A
+{
+    public partial class MainPage : ContentPage
+    {
+        bool CambioColor = true;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            dtpFechaNacimiento.MaximumDate = DateTime.Today;
+
+            
+
+        }
+
+        private void btnGirar_Clicked(object sender, EventArgs e)
+        {
+            if (CambioColor)
+                lblPrueba.TextColor = Color.Blue;
+            else
+                lblPrueba.TextColor = Color.Red;
+            
+            CambioColor=!CambioColor;
+            
+        }
+    }
+}
